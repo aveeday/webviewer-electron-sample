77d5f6be1400f0863fc149e5795fad7c853ab1db
Konstantin <kostya.nesterenko@gmail.com>
2021-06-17 13:43:41 +0300

    reload viewer

--- src/render.js
+++ src/render.js
@@ -2,76 +2,94 @@ const { dialog } = require('electron').remote;
 const path = require('path');
 const fs = require('fs');
 
-const viewerElement = document.getElementById('viewer');
+let id = 'viewer';
+
+const viewerElement = document.getElementById(id);
 
 const openFileBtn = document.getElementById('open');
 const saveFileBtn = document.getElementById('save');
 const closeFileBtn = document.getElementById('close');
+const newViewerBtn = document.getElementById('new');
 
-WebViewer(
-  {
-    path: '../public/lib',
-    initialDoc: '../public/files/webviewer-demo-annotated.pdf',
-  },
-  viewerElement,
-).then(instance => {
-  // Interact with APIs here.
-  // See https://www.pdftron.com/documentation/web for more info
-  instance.setTheme('dark');
-  instance.disableElements(['downloadButton']);
+init(viewerElement);
 
-  const { docViewer, annotManager } = instance;
+newViewerBtn.onclick = () => {
+  const viewerElement = document.getElementById(id);
+  viewerElement.remove();
+  const newViewer = document.createElement('div');
+  id = 'viewer' + Math.random().toString();
+  newViewer.id = id;
+  newViewer.classList = ['webviewer'];
+  document.getElementById('container').appendChild(newViewer);
+  init(newViewer);
+};
 
-  openFileBtn.onclick = async () => {
-    const file = await dialog.showOpenDialog({
-      properties: ['openFile', 'multiSelections'],
-      filters: [
-        { name: 'Documents', extensions: ['pdf', 'docx', 'pptx', 'xlsx'] },
-        { name: 'Images', extensions: ['png', 'jpg'] },
-      ],
-    });
+function init(viewerElement) {
+  WebViewer(
+    {
+      path: '../public/lib',
+      initialDoc: '../public/files/webviewer-demo-annotated.pdf',
+    },
+    viewerElement,
+  ).then(instance => {
+    // Interact with APIs here.
+    // See https://www.pdftron.com/documentation/web for more info
+    instance.setTheme('dark');
+    instance.disableElements(['downloadButton']);
 
-    if (!file.canceled) {
-      instance.loadDocument(file.filePaths[0]);
-    }
-  };
+    const { docViewer, annotManager } = instance;
 
-  saveFileBtn.onclick = async () => {
-    const file = await dialog.showOpenDialog({
-      title: 'Select where you want to save the PDF',
-      buttonLabel: 'Save',
-      filters: [
-        {
-          name: 'PDF',
-          extensions: ['pdf'],
-        },
-      ],
-      properties: ['openDirectory'],
-    });
-
-    if (!file.canceled) {
-      const doc = docViewer.getDocument();
-      const xfdfString = await annotManager.exportAnnotations();
-      const data = await doc.getFileData({
-        // saves the document with annotations in it
-        xfdfString,
+    openFileBtn.onclick = async () => {
+      const file = await dialog.showOpenDialog({
+        properties: ['openFile', 'multiSelections'],
+        filters: [
+          { name: 'Documents', extensions: ['pdf', 'docx', 'pptx', 'xlsx'] },
+          { name: 'Images', extensions: ['png', 'jpg'] },
+        ],
       });
-      const arr = new Uint8Array(data);
 
-      fs.writeFile(
-        `${file.filePaths[0].toString()}/annotated.pdf`,
-        arr,
-        function (err) {
-          if (err) throw err;
-          console.log('Saved!');
-        },
-      );
-    }
-  };
+      if (!file.canceled) {
+        instance.loadDocument(file.filePaths[0]);
+      }
+    };
 
-  closeFileBtn.onclick = () => {
-    instance.closeDocument().then(function () {
-      console.log('Document is closed');
-    });
-  };
-});
+    saveFileBtn.onclick = async () => {
+      const file = await dialog.showOpenDialog({
+        title: 'Select where you want to save the PDF',
+        buttonLabel: 'Save',
+        filters: [
+          {
+            name: 'PDF',
+            extensions: ['pdf'],
+          },
+        ],
+        properties: ['openDirectory'],
+      });
+
+      if (!file.canceled) {
+        const doc = docViewer.getDocument();
+        const xfdfString = await annotManager.exportAnnotations();
+        const data = await doc.getFileData({
+          // saves the document with annotations in it
+          xfdfString,
+        });
+        const arr = new Uint8Array(data);
+
+        fs.writeFile(
+          `${file.filePaths[0].toString()}/annotated.pdf`,
+          arr,
+          function (err) {
+            if (err) throw err;
+            console.log('Saved!');
+          },
+        );
+      }
+    };
+
+    closeFileBtn.onclick = () => {
+      instance.closeDocument().then(function () {
+        console.log('Document is closed');
+      });
+    };
+  });
+}
